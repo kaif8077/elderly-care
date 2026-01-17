441198130bcc9a0712c29bb220dd571c3d40169d
Mohammad Kaif <mohdkaif90275@gmail.com>
2026-01-18 02:00:32 +0530

    build: successful local build

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: 'https://kaif8077-elderly-care.vercel.app/' || 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
